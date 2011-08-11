a22310dd9686e8cca365cf17a578b5f7f333cfea
David Petersen <thedjpetersen@gmail.com>
2011-08-11 14:11:44 -0700

    Adding api support

--- urls.py
+++ urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from tastypie.api import Api
 from django.contrib import admin
+
+from api import BoxResource, LeafResource, PersonResource
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^api/', include('data_store.client_api_urls')),
+v1_api = Api(api_name='v1')
+v1_api.register(BoxResource())
+v1_api.register(LeafResource())
+v1_api.register(PersonResource())
 
+urlpatterns = patterns('',
+    #url(r'^api/', include('data_store.client_api_urls')),
+    (r'^api/', include(v1_api.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
