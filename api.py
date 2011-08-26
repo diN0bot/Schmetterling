9315a97bdfbff7d65470be5285b4ce566cbe3b43
David Petersen <thedjpetersen@gmail.com>
2011-08-25 23:31:12 -0700

    Added proper authorization

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
-from tastypie.authorization import Authorization
+from django.contrib.auth.models import User
+from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import DjangoAuthorization
+#from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from data_store.models import Box, Leaf, Person
 
@@ -6,17 +9,26 @@ from data_store.models import Box, Leaf, Person
 class BoxResource(ModelResource):
     class Meta:
             queryset = Box.objects.all()
-            authorization = Authorization()
+            #authorization = Authorization()
+            # Add it here.
+            authentication = BasicAuthentication()
+            authorization = DjangoAuthorization()
 
 class LeafResource(ModelResource):
     class Meta:
             queryset = Leaf.objects.all()
             limit = 100000
-            authorization = Authorization()
             default_format = 'application/json'
+            #authorization = Authorization()
+            # Add it here.
+            authentication = BasicAuthentication()
+            authorization = DjangoAuthorization()
 
 class PersonResource(ModelResource):
     class Meta:
             queryset = Person.objects.all()
-            authorization = Authorization()
+            #authorization = Authorization()
+            # Add it here.
+            authentication = BasicAuthentication()
+            authorization = DjangoAuthorization()
 
